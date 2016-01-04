407d723bcea4fafc2c96f1bc7699031e972a2391
demondayza <demondayza@users.noreply.github.com>
2016-01-04 20:34:01 +0200

    Some Extra Validation and Variable Cleaning!
    Just that little bit extra to get rid of common XSS Vectors.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -10,10 +10,10 @@ if(empty($_POST['name'])  		||
 	return false;
    }
 	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
 $to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
