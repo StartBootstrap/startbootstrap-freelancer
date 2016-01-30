b55306b62e8cd4db4a1da7777921e339171c074a
David Miller <hello@davidmiller.io>
2016-01-30 14:20:42 -0500

    dropdown fix

--- js/freelancer.js
+++ js/freelancer.js
@@ -32,6 +32,6 @@ $('body').scrollspy({
 })
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+$('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
     $('.navbar-toggle:visible').click();
 });
