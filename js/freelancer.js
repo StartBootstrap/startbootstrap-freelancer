5043b2b0b7c25cbcf0a2037e95c44a99f1a075b8
Rikesh Ramlochund <rrikesh@gmail.com>
2016-04-27 08:10:22 +0400

    Validation issues: Missing semicolons

--- js/freelancer.js
+++ js/freelancer.js
@@ -36,7 +36,7 @@ $(function() {
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
-})
+});
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
