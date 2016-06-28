381aba4232f6f5e814ce5cb816db5719828b1842
David Miller <hello@davidmiller.io>
2016-06-27 20:38:38 -0400

    updated version

--- js/freelancer.min.js
+++ js/freelancer.min.js
@@ -1,5 +1,5 @@
 /*!
- * Start Bootstrap - Freelancer v1.0.6 (http://startbootstrap.com/template-overviews/freelancer)
+ * Start Bootstrap - Freelancer v1.1.0 (http://startbootstrap.com/template-overviews/freelancer)
  * Copyright 2013-2016 Start Bootstrap
  * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
  */
