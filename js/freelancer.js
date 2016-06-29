4acf227e584cf5a29efc6c9848d2a7da92f95d3d
David Miller <hello@davidmiller.io>
2016-06-29 17:36:00 -0400

    fixed script for scrolling

--- js/freelancer.js
+++ js/freelancer.js
@@ -4,7 +4,7 @@
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('page-scroll a').bind('click', function(event) {
+    $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
