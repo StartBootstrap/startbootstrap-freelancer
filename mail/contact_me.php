d5d0fa325cde37699cf85b0d56fcfef6fb19ae42
Aaron Horler <aghorler@users.noreply.github.com>
2017-01-20 23:48:33 +1100

    Very minor changes.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = ''; // Removed from source for privacy reasons.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message\n\nIP address: " . $_SERVER['REMOTE_ADDR'];
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message\n\nIP address: " . $_SERVER['REMOTE_ADDR'];
 $headers = "From: noreply@aaronhorler.com\n";
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
