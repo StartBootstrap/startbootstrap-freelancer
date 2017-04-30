a6c06686c28bff7588a17bc25e586e69a71c6b5f
Aaron Horler <aghorler@users.noreply.github.com>
2017-04-30 19:57:37 +1000

    OpenPGP.js contact form, and new articles.
    Plus a few minor changes.

--- js/contact_me.js
+++ js/contact_me.js
@@ -13,7 +13,6 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
