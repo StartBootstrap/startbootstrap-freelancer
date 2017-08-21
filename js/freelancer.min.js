a5989d4868e3fa277da028a561e4f134d6f4e792
David Miller <hello@davidmiller.io>
2017-08-21 19:48:26 -0400

    update package.json and run gulp

--- js/freelancer.min.js
+++ js/freelancer.min.js
@@ -1,5 +1,5 @@
 /*!
- * Start Bootstrap - Freelancer v4.0.0-beta (http://startbootstrap.com/template-overviews/freelancer)
+ * Start Bootstrap - Freelancer v4.0.0-beta (https://startbootstrap.com/template-overviews/freelancer)
  * Copyright 2013-2017 Start Bootstrap
  * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
  */
