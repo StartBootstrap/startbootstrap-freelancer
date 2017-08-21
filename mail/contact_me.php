9a49ae230eeb82c02f02de7f511f261732aa7d19
Admin Localnet <localnet@users.noreply.github.com>
2017-08-21 19:33:32 +0200

    Return the mail function result.
    http://php.net/manual/en/function.mail.php
    
    > Returns TRUE if the mail was successfully accepted for delivery, FALSE otherwise.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,10 +1,10 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
+if(empty($_POST['name'])	||
+   empty($_POST['email'])	||
+   empty($_POST['phone'])	||
    empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
 	return false;
@@ -21,6 +21,6 @@ $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+
+return mail($to, $email_subject, $email_body, $headers);			
 ?>
