0f40f5ced3616aa0ce0dfd78d6c1bc6eca1d60c2
Nathanael Yusuf Tj <nytj@ntechp.id>
2019-01-09 20:06:57 +0700

    Moving webfont directory

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,7 +3,7 @@ let mix = require('laravel-mix');
 mix.js('src/js/app.js', 'dist/js')
    .sass('src/scss/vendor.scss', 'dist/css')
    .sass('src/scss/freelancer.scss', 'dist/css')
-   .copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'webfonts')
+   .copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'dist/webfonts')
    .autoload({
 	    jQuery: 'jquery',
 	    $: 'jquery',
