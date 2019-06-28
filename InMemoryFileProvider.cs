739d73ad487ee6cd2e41650715b520084a060412
Petr Švihlík <svihlik.petr@gmail.com>
2019-06-28 19:49:11 +0200

    config

---
+++ InMemoryFileProvider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.IO;
+using System.Text;
+
+namespace StartBootstrap.Freelancer.Blazor
+{
+    public class InMemoryFileProvider : IFileProvider
+    {
+        private class InMemoryFile : IFileInfo
+        {
+            private readonly byte[] _data;
+            public InMemoryFile(string json) => _data = Encoding.UTF8.GetBytes(json);
+            public Stream CreateReadStream() => new MemoryStream(_data);
+
+            public bool Exists { get; } = true;
+            public long Length => _data.Length;
+            public string PhysicalPath { get; } = string.Empty;
+            public string Name { get; } = string.Empty;
+            public DateTimeOffset LastModified { get; } = DateTimeOffset.UtcNow;
+            public bool IsDirectory { get; } = false;
+        }
+
+        private readonly IFileInfo _fileInfo;
+        public InMemoryFileProvider(string json) => _fileInfo = new InMemoryFile(json);
+        public IFileInfo GetFileInfo(string _) => _fileInfo;
+        public IDirectoryContents GetDirectoryContents(string _) => null;
+        public IChangeToken Watch(string _) => NullChangeToken.Singleton;
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -3,8 +3,9 @@ using Microsoft.Extensions.DependencyInjection;
 using KenticoCloud.Delivery;
 using StartBootstrap.Freelancer.Blazor.Models;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
 using System;
+using System.Linq;
+using System.IO;
 
 namespace StartBootstrap.Freelancer.Blazor
 {
@@ -12,25 +13,38 @@ namespace StartBootstrap.Freelancer.Blazor
     {
         public void ConfigureServices(IServiceCollection services)
         {
-           // services.AddSingleton<ITypeProvider, CustomTypeProvider>();
+            services.AddSingleton<ITypeProvider, CustomTypeProvider>();
 
-            //var configuration = new ConfigurationBuilder()
-            //            .AddJsonFile("appsettings.json", true)
-            //            .Build();
-
-            //// Fill the DI container
-            //services.Configure<DeliveryOptions>(configuration);
-            //services.AddDeliveryClient(configuration);
-
-            //TODO configuration
-           // var client = DeliveryClientBuilder.WithProjectId("a9b221f5-7375-01af-4d91-67a87cb4e991").WithTypeProvider(new CustomTypeProvider()).Build();
-
-            //services.AddSingleton<IDeliveryClient>(client);
-            
+            services.AddSingleton((s) =>
+            {
+                return GetConfiguration();
+            });
         }
 
+        private IConfiguration GetConfiguration()
+        {
+            System.Reflection.Assembly assembly = GetType().Assembly;
+            var resource = assembly.GetName().Name + ".Configuration.appsettings.json";
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+
+            if (assembly.GetManifestResourceNames().Contains(resource))
+            {
+                string result = null;
+                using (Stream stream = assembly.GetManifestResourceStream(resource))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    result = reader.ReadToEnd();
+                }
+
+                configurationBuilder.AddJsonFile(new InMemoryFileProvider(result), resource, false, false);
+            }
+
+            return configurationBuilder.Build();
+        }
+
+
         public void Configure(IComponentsApplicationBuilder app)
-        {            
+        {
             app.AddComponent<App>("app");
         }
     }
