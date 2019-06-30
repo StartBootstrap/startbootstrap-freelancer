7b1145b98d0b77f11a4a2591f2f7a4a4514b7bdd
Petr Švihlík <svihlik.petr@gmail.com>
2019-06-30 12:42:56 +0200

    sending emails scaffold

---
+++ Models/ContactForm.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StartBootstrap.Freelancer.Blazor.Models
+{
+    public class ContactForm
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+
+        public string Success { get; set; }
+
+        public string Error { get; set; }
+    }
+}
--- Shared/Contact.razor.cs
+++ Shared/Contact.razor.cs
@@ -1,13 +1,41 @@
 ﻿using Microsoft.AspNetCore.Components;
+using StartBootstrap.Freelancer.Blazor.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace StartBootstrap.Freelancer.Blazor.Shared
 {
     public class ContactBase : ComponentBase
     {
-        //TODO email
+        protected ContactForm ContactForm = new ContactForm();
+
+        protected void HandleValidSubmit()
+        {
+            ContactForm.Success = null;
+            ContactForm.Error = null;
+            try
+            {
+                SmtpClient client = new SmtpClient("mysmtpserver");
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential("username", "password");
+
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.From = new MailAddress(ContactForm.Email);
+                mailMessage.To.Add("receiver@me.com");
+                mailMessage.Body = ContactForm.Message + $"<br />Phone: {ContactForm.Phone}";
+                mailMessage.Subject = $"Message from {ContactForm.Name}";
+                client.Send(mailMessage);
+                ContactForm.Success = "Message sent!";
+
+            }
+            catch (Exception ex)
+            {
+                ContactForm.Error = ex.Message;
+            }
+            StateHasChanged();
+        }
+
     }
 }
