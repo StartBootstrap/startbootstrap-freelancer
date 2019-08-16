363aa649cad174c6ac870ed8da83e22296507a37
Mike Krueger <mjkrueger@yahoo.com>
2019-08-16 14:15:37 -0400

    refactor website to get navbar working and remove references to rental property

---
+++ js/customize/troop706.js
@@ -0,0 +1,4 @@
+    /*makes the header image (baldy.jpg) stretch across entire page - seems to work without this too*/
+    $(document).ready(function(){
+      $('.header').height($(window).height());
+    })
