70d1c8b354dcc8d8eb3a9431b60765d51cc4d7f9
David Miller <hello@davidmiller.io>
2019-09-16 17:26:29 -0400

    update dependencies and autoprefixer config

--- gulpfile.js
+++ gulpfile.js
@@ -81,7 +81,6 @@ function css() {
     }))
     .on("error", sass.logError)
     .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
       cascade: false
     }))
     .pipe(header(banner, {
