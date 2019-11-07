a9550ea3825c36d6511a6e31c37f697e2a1e6456
Petr Švihlík <svihlik.petr@gmail.com>
2019-11-07 21:54:27 +0100

    automatic model regeneration

--- src/GenerateModels.ps1
+++ src/GenerateModels.ps1
@@ -1,1 +1,2 @@
-﻿KontentModelGenerator -p "a9b221f5-7375-01af-4d91-67a87cb4e991" -o "C:\Users\rocky\Documents\GitHub\StartBootstrap.Freelancer.Blazor\Models" -n "StartBootstrap.Freelancer.Blazor.Models" -g
+﻿dotnet tool restore
+KontentModelGenerator -p "a9b221f5-7375-01af-4d91-67a87cb4e991" -o ".\Models" -n "StartBootstrap.Freelancer.Blazor.Models" -g
