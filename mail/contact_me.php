c71cc134a4c8fbd8ab24903b23df8389537a931b
szambad <szambad@thincr-tech.com>
2019-11-18 16:19:31 +0530

    szambad
    Thincr-tech

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
 $header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
-
+szambad@thincr-tech.com
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
 ?>
