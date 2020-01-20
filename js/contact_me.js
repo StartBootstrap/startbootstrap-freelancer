89e279c24e975a331c7d1a440cdc47c8cf368fc7
Danielxburns <41316262+Danielxburns@users.noreply.github.com>
2020-01-20 01:00:48 -0600

    changed color of carousel indicators. Made minor changes to contact-me and undated description in ICLayout modal

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,6 +1,5 @@
 $(function() {
-
-  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+  $('#contactForm input,#contactForm textarea').jqBootstrapValidation({
     preventSubmit: true,
     submitError: function($form, event, errors) {
       // additional error messages or events
@@ -8,20 +7,23 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
-      var name = $("input#name").val();
-      var email = $("input#email").val();
-      var phone = $("input#phone").val();
-      var message = $("textarea#message").val();
+      var name = $('input#name').val();
+      var email = $('input#email').val();
+      var phone = $('input#phone').val();
+      var message = $('textarea#message').val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
       if (firstName.indexOf(' ') >= 0) {
-        firstName = name.split(' ').slice(0, -1).join(' ');
+        firstName = name
+          .split(' ')
+          .slice(0, -1)
+          .join(' ');
       }
-      $this = $("#sendMessageButton");
-      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+      $this = $('#sendMessageButton');
+      $this.prop('disabled', true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
-        type: "POST",
+        url: '././mail/contact_me.php',
+        type: 'POST',
         data: {
           name: name,
           phone: phone,
@@ -32,40 +34,52 @@ $(function() {
         success: function() {
           // Success message
           $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
-          $('#success > .alert-success')
-            .append('</div>');
+            .html(
+              "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+            )
+            .append('</button>');
+          $('#success > .alert-success').append(
+            '<strong>Your message has been sent. </strong>'
+          );
+          $('#success > .alert-success').append('</div>');
           //clear all fields
-          $('#contactForm').trigger("reset");
+          $('#contactForm').trigger('reset');
         },
         error: function() {
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger')
+            .html(
+              "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+            )
+            .append('</button>');
+          $('#success > .alert-danger').append(
+            $('<strong>').text(
+              'Sorry ' +
+                firstName +
+                ', it seems that my mail server is not responding. To use your default mail client click the envelope in the navbar at to top of the page.'
+            )
+          );
           $('#success > .alert-danger').append('</div>');
           //clear all fields
-          $('#contactForm').trigger("reset");
+          $('#contactForm').trigger('reset');
         },
         complete: function() {
           setTimeout(function() {
-            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+            $this.prop('disabled', false); // Re-enable submit button when AJAX call is complete
           }, 1000);
         }
       });
     },
     filter: function() {
-      return $(this).is(":visible");
-    },
+      return $(this).is(':visible');
+    }
   });
 
-  $("a[data-toggle=\"tab\"]").click(function(e) {
+  $('a[data-toggle="tab"]').click(function(e) {
     e.preventDefault();
-    $(this).tab("show");
+    $(this).tab('show');
   });
 });
 
