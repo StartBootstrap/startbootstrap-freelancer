6b03d03fedfc1a8ad3b0dd1f7775ddffa6328d44
David Miller <hello@davidmiller.io>
2020-03-16 18:07:13 -0400

    v6 reorganization and dev environment update
    moved to cdn for third party assets, changed project structure to have src and dist directoires, moved to npm scripts based dev environment dropping the gulp dependencies

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "contact_me.php",
         type: "POST",
         data: {
           name: name,
