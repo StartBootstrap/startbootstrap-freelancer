b7ce9502b659e0aeb495bca767cf2a3daf32a177
Elia <elia.shreidler@gmail.com>
2020-05-13 11:03:41 +0300

    render all pub files on change..

--- scripts/sb-watch.js
+++ scripts/sb-watch.js
@@ -63,15 +63,7 @@ function _processFile(filePath, watchEvent) {
 }
 
 function _handlePug(filePath, watchEvent) {
-    if (watchEvent === 'change') {
-        if (filePath.match(/includes/) || filePath.match(/mixins/) || filePath.match(/\/pug\/layouts\//)) {
-            return _renderAllPug();
-        }
-        return renderPug(filePath);
-    }
-    if (!filePath.match(/includes/) && !filePath.match(/mixins/) && !filePath.match(/\/pug\/layouts\//)) {
-        return renderPug(filePath);
-    }
+    return _renderAllPug();
 }
 
 function _renderAllPug() {
