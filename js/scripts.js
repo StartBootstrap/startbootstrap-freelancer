6c8f78358ef5102091ea6a313254f4f0ca26f2bb
David Miller <hello@davidmiller.io>
2021-04-01 19:02:15 -0400

    run build

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 /*!
-    * Start Bootstrap - Freelancer v6.0.6 (https://startbootstrap.com/theme/freelancer)
-    * Copyright 2013-2021 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-    */
-    (function ($) {
+* Start Bootstrap - Freelancer v6.0.6 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+*/
+(function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using anime.js
