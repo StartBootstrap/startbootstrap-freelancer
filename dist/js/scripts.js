171d87e7583c96cb6d93c97828c3bdddfa11e263
sar.yoon89@gmail.com <sar.yoon89@gmail.com>
2024-03-17 00:43:38 +0100

    index.html changes, pics changed

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2023 Start Bootstrap
+* Copyright 2013-2024 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
 */
 //
